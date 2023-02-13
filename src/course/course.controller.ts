390cd4c94ed5f4b11f17d07a86dcd650871d36e0
Nathnael Dereje <nathandere1357@gmail.com>
2023-02-13 12:28:12 +0300

    resolve conflicting changes

--- src/course/course.controller.ts
+++ src/course/course.controller.ts
@@ -17,42 +17,25 @@ export class CourseController {
         return this.courseService.getCourse(userId);
     }
 
-<<<<<<< HEAD
-    @Get('/:id')
-    getourseById(@GetUser('id') userId: number, @Param('id', ParseIntPipe) courseId: number){
-=======
     @Get('getCourse')
     getcourseByName(@GetUser('id') userId: number, @Param('id', ParseIntPipe) courseId: number){
->>>>>>> 5962f595bef23cbc23f9675b5f5d8f7797ecf824
         return this.courseService.getCourseById(userId, courseId);
 
     }
 
-<<<<<<< HEAD
-    @Post('add')    
-=======
     @Post('addCourse')
->>>>>>> 5962f595bef23cbc23f9675b5f5d8f7797ecf824
     addCourse(@GetUser('id') userId: number, @Body() dto: AddCourseDto){
         return this.courseService.addCourse(userId, dto);
 
     }
 
-<<<<<<< HEAD
-    @Patch('/:id')
-=======
     @Patch('editCourse')
->>>>>>> 5962f595bef23cbc23f9675b5f5d8f7797ecf824
     editCourseById(@GetUser('id') userId: number, @Param('id', ParseIntPipe) courseId: number, @Body() dto: EditCourseDto){
         return this.courseService.editCourseById(userId, courseId, dto);
 
     }
 
-<<<<<<< HEAD
-    @Delete('/:id')
-=======
     @Delete('deleteCourse')
->>>>>>> 5962f595bef23cbc23f9675b5f5d8f7797ecf824
     removeCourseById(@GetUser('id') userId: number, @Param('id', ParseIntPipe) courseId: number){
         return this.courseService.removeCourseById(userId, courseId);
 
